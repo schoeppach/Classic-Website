b911400fb641df1abd960bcc6024f485deea3cb9
schoeppach <spoon@mail.de>
2023-04-12 23:49:39 +0200

    7.3

--- evaluation.php
+++ evaluation.php
@@ -36,6 +36,10 @@
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             
               <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="home.html">Home</a>
+              </li>
+
+              <li class="nav-item">
                 <a class="nav-link" aria-current="page" href="comingsoon.html">Newsletter</a>
               </li>
 
